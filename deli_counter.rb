a7bed75dbd859d8504233575a21611e800614916
Damien Nelson <dhnisaman@gmail.com>
2023-04-13 11:35:40 -0400

    commit tree 0060ab5d7d1f3b3cc1d18c07acf97d00c37ef587

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+require 'pry'
+
+
+katz_deli = []
+
+def line(deli) 
+    if deli.empty?
+        puts "The line is currently empty."
+    else
+       line = "The line is currently:"
+       deli.each_with_index {|place, index| line << " #{index + 1}. #{place}"}
+       puts line
+    end
+end
+
+def take_a_number(deli, name)
+    deli << name
+    puts "Welcome, #{name}. You are number #{deli.length} in line."
+end
+
+def now_serving(deli)
+    puts "Currently serving #{deli.first}." unless deli.empty?
+    puts "There is nobody waiting to be served!" if deli.empty?
+    deli.shift
+end
+
